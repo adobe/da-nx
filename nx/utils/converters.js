8e38f0a699fc62a31a0723216b41dfd89e159e23
Okan Sahin <osahin@adobe.com>
2025-11-25 12:12:38 +0100

    Rewrite hlx references to aem

--- nx/utils/converters.js
+++ nx/utils/converters.js
@@ -151,6 +151,7 @@ export function mdToDocDom(md) {
 
   let htmlText = toHtml(hast);
   htmlText = htmlText.replaceAll('.hlx.page', '.hlx.live');
+  htmlText = htmlText.replaceAll('.hlx.live', '.aem.live');
   htmlText = htmlText.replaceAll('.aem.page', '.aem.live');
 
   const parser = new DOMParser();
