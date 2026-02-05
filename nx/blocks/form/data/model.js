baa17ae2e278ec3fb654103825f90e08e5d2eee4
Chris Millar <cmillar@adobe.com>
2026-02-05 05:57:56 -0800

    DA Structured Content
    * Introduce a form block to build a form from a JSON schema
    * Created a new dependency to build form dependencies

---
+++ nx/blocks/form/data/model.js
@@ -0,0 +1,92 @@
+import { DA_ORIGIN } from 'https://da.live/blocks/shared/constants.js';
+import { daFetch } from 'https://da.live/blocks/shared/utils.js';
+import HTMLConverter from '../utils/html2json.js';
+import JSONConverter from '../utils/json2html.js';
+import { Validator } from '../../../deps/da-form/dist/index.js';
+import { annotateProp, setValueByPath } from '../utils/utils.js';
+
+/**
+ * A data model that represents a piece of structured content.
+ */
+export default class FormModel {
+  constructor({ path, html, json, schemas }) {
+    if (!(html || json)) {
+      // eslint-disable-next-line no-console
+      console.log('Please supply JSON or HTML to make a form model');
+      return;
+    }
+
+    if (html) {
+      this._html = html;
+      this.updateJson();
+    } else if (json) {
+      this._json = json;
+      this.updateHtml();
+    }
+
+    this._path = path;
+    this._schemas = schemas;
+    this._schema = schemas[this._json.metadata.schemaName];
+    this._annotated = annotateProp('data', this._json.data, this._schema, this._schema);
+  }
+
+  clone() {
+    return new FormModel({
+      path: this._path,
+      html: this._html,
+      json: JSON.parse(JSON.stringify(this._json)), // Deep copy of JSON
+      schemas: this._schemas, // or clone this too if needed
+    });
+  }
+
+  validate() {
+    const validator = new Validator(this._schema, '2020-12');
+    return validator.validate(this._json.data);
+  }
+
+  updateJson() {
+    const converter = new HTMLConverter(this._html);
+    this._json = converter.json;
+  }
+
+  updateHtml() {
+    const html = JSONConverter(this._json);
+    this._html = html;
+  }
+
+  updateProperty({ name, value }) {
+    setValueByPath(this._json, name, value);
+    this.updateHtml();
+  }
+
+  async saveHtml() {
+    const body = new FormData();
+    const data = new Blob([this._html], { type: 'text/html' });
+    body.append('data', data);
+
+    const opts = { method: 'POST', body };
+
+    // TODO: Don't assume the save went perfect
+    await daFetch(`${DA_ORIGIN}/source${this._path}`, opts);
+  }
+
+  set html(html) {
+    this._html = html;
+  }
+
+  get html() {
+    return this._html;
+  }
+
+  get annotated() {
+    return this._annotated;
+  }
+
+  get schema() {
+    return this._schema;
+  }
+
+  get json() {
+    return this._json;
+  }
+}
---
+++ nx/blocks/form/utils/json2html.js
@@ -0,0 +1,137 @@
+function getDocument() {
+  const doc = document.implementation.createHTMLDocument();
+
+  const header = document.createElement('header');
+
+  const main = document.createElement('main');
+  const section = document.createElement('div');
+  main.append(section);
+
+  const footer = document.createElement('footer');
+
+  doc.body.append(header, main, footer);
+
+  return doc;
+}
+
+function createRow(key, valCol) {
+  const row = document.createElement('div');
+
+  const keyCol = document.createElement('div');
+  const keyPara = document.createElement('p');
+  keyPara.textContent = key;
+  keyCol.append(keyPara);
+
+  row.append(keyCol, valCol);
+  return row;
+}
+
+function createBlock(name) {
+  const block = document.createElement('div');
+  block.className = name;
+  return block;
+}
+
+function createNestedBlock(key, obj, nestedBlocks) {
+  const guid = Math.random().toString(36).substring(2, 8);
+  const nestedBlock = createBlock(`${key} ${key}-${guid}`);
+  const rows = Object.entries(obj).map(([k, v]) => {
+    // eslint-disable-next-line no-use-before-define
+    const nestedValCol = createValueCol(k, v, nestedBlocks);
+    return createRow(k, nestedValCol);
+  });
+  nestedBlock.append(...rows);
+  nestedBlocks.push(nestedBlock);
+  return guid;
+}
+
+function createValueCol(key, value, nestedBlocks) {
+  const valCol = document.createElement('div');
+
+  if (value) {
+    // Create a paragraph to hold the property
+    const valPara = document.createElement('p');
+
+    // Handle objects by creating a nested block
+    if (typeof value === 'object') {
+      // Check if value is an array and create multiple nested blocks if needed
+      if (Array.isArray(value)) {
+        // If it's an array of objects, create a nested block for each object
+        const ul = document.createElement('ul');
+        if (!value.length) {
+          const li = document.createElement('li');
+          ul.append(li);
+        }
+        value.forEach((item) => {
+          if (typeof item === 'object' && item !== null) {
+            const guid = createNestedBlock(key, item, nestedBlocks);
+            const li = document.createElement('li');
+            li.textContent = `self://#${key}-${guid}`;
+            ul.append(li);
+          } else {
+            // If the array entry is a primitive, treat accordingly
+            const li = document.createElement('li');
+            li.textContent = item;
+            ul.append(li);
+          }
+        });
+        valCol.append(ul);
+        // Since we already appended paragraphs above, skip the rest of this function
+        return valCol;
+      }
+
+      // handle objects
+      const guid = createNestedBlock(key, value, nestedBlocks);
+      valPara.textContent = `self://#${key}-${guid}`;
+    } else {
+      valPara.textContent = value;
+    }
+
+    valCol.append(valPara);
+  }
+
+  return valCol;
+}
+
+function getFormBlock(metadata, nestedBlocks) {
+  const daForm = createBlock('da-form');
+
+  const rows = Object.entries(metadata).map((entry) => {
+    const [key, value] = entry;
+    const xKey = key === 'schemaName' ? 'x-schema-name' : key;
+
+    const valCol = createValueCol(key, value, nestedBlocks);
+
+    return createRow(xKey, valCol);
+  });
+
+  daForm.append(...rows);
+  return daForm;
+}
+
+function getDataBlock(schemaName, data, nestedBlocks) {
+  const dataBlock = createBlock(schemaName);
+  const rows = Object.entries(data).map((entry) => {
+    const [key, value] = entry;
+
+    const valCol = createValueCol(key, value, nestedBlocks);
+
+    return createRow(key, valCol);
+  });
+  dataBlock.append(...rows);
+  return dataBlock;
+}
+
+export default function json2html(json) {
+  const nestedBlocks = [];
+  const doc = getDocument();
+
+  const { metadata, data } = json;
+  const { schemaName } = metadata;
+  const formBlock = getFormBlock(metadata, nestedBlocks);
+  const dataBlock = getDataBlock(schemaName, data, nestedBlocks);
+
+  doc.querySelector('main > div').append(formBlock, dataBlock, ...nestedBlocks);
+
+  return doc.body.outerHTML;
+}
