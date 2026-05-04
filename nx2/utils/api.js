1bddd9b2476e4e73651d2da2312a794408b7f238
Chris Millar <chris@millr.org>
2026-05-03 20:53:22 -0700

    minor: hlx6 listSource

--- nx2/utils/api.js
+++ nx2/utils/api.js
@@ -85,11 +85,17 @@ const getApiPath = async (org, site, api, daPath) => {
 
   if (api === 'config') {
     if (hlx6) {
-      if (!site) return `${AEM_API}/${api}/${org}.json`;
-      return `${AEM_API}/${api}/${org}/${site}.json`;
+      if (!site) return `${AEM_API}/${org}/config.json`;
+      return `${AEM_API}/${org}/sites/${site}/config.json`;
     }
     if (!site) return `${DA_ADMIN}/${api}/${org}/`;
     return `${DA_ADMIN}/${api}/${org}/${site}/`;
+  }
+
+  // HLX6 does not have a list api,
+  // it will use the source formatting.
+  if (api === 'list') {
+    return `${DA_ADMIN}/${api}/${org}/${site}${daPath}`;
   }
 
   // api === 'source'
@@ -133,6 +139,17 @@ export const getSource = async ({ org, site, daPath }) => {
   return daFetch({ url });
 };
 
+export const listSource = async ({ org, site, daPath }) => {
+  const hlx6 = await isHlx6(org, site);
+  if (hlx6) {
+    const path = daPath ? `/${daPath}/` : '/';
+    const url = await getApiPath(org, site, 'source', path);
+    return daFetch({ url });
+  }
+  const url = await getApiPath(org, site, 'list', daPath);
+  return daFetch({ url });
+};
+
 export const putVersion = async ({ org, site, daPath, operation, comment }) => {
   const url = new URL(await getApiPath(org, site, 'versionsource', daPath));
   if (operation) url.searchParams.set('operation', operation);
@@ -156,3 +173,37 @@ export const putConfig = async ({ org, site, body }) => {
 
   return daFetch({ url, opts });
 };
+
+export function hlx6ToDaList(parentPath, items) {
+  return items.map((item) => {
+    const contentType = item['content-type'];
+
+    // Only HLX6 has a content type
+    if (!contentType) return item;
+
+    // Normalize folder
+    const isFolder = item.name.endsWith('/');
+    let name = isFolder ? item.name.slice(0, -1) : item.name;
+
+    // Set the path before extension removal
+    const path = `${parentPath}/${name}`;
+
+    // Remove extension for display
+    const nameSplit = name.split('.');
+    name = nameSplit.length > 1 ? nameSplit[0] : name;
+
+    // Scaffold out the basics
+    const daItem = { name, path, contentType };
+
+    const ext = nameSplit.length > 1 && nameSplit.pop();
+    if (ext) daItem.ext = ext;
+
+    const lastModified = item['last-modified'];
+    if (lastModified) {
+      const unixTime = Math.floor(new Date(lastModified).getTime());
+      daItem.lastModified = unixTime;
+    }
+
+    return daItem;
+  });
+}
