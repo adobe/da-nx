3f0afa8de3c0c6552234ee1cc2dc79e207d38a29
Adrian Kozma <kozma@adobe.com>
2026-05-11 14:03:39 +0200

    ref 4 remove v2

--- nx/blocks/form-v2/services/loader/document-loader.js
+++
@@ -1,9 +0,0 @@
-import { fetchSourceHtml } from '../persistence/json-api.js';
-
-export async function loadDocumentHtml(details) {
-  if (!details?.sourceUrl) {
-    return { error: 'Missing source URL.' };
-  }
-
-  return fetchSourceHtml({ sourceUrl: details.sourceUrl });
-}
