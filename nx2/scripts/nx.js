936145d4db65970eb32e56c5626f57e5e7089600
Hannes Hertach <hhertach@adobe.com>
2026-05-20 11:14:33 +0200

    remove style file

--- nx2/scripts/nx.js
+++ nx2/scripts/nx.js
@@ -10,7 +10,6 @@
  * governing permissions and limitations under the License.
  */
 
-export { loadStyle } from '../utils/style.js';
 const NX_BLOCKS = new Set([
   'importer',
 ]);
@@ -320,3 +319,28 @@ export async function loadArea({ area } = { area: document }) {
     await restorePanels();
   }
 }
+
+const cache = {};
+
+// eslint-disable-next-line import/prefer-default-export
+export const loadStyle = (supplied) => {
+  // Convenience replacement for WCs
+  const path = supplied.replace('.js', '.css');
+
+  try {
+    cache[path] ??= new Promise((resolve) => {
+      (async () => {
+        const resp = await fetch(path);
+        const text = await resp.text();
+        const sheet = new CSSStyleSheet({ baseURL: path });
+        sheet.path = path;
+        sheet.replaceSync(text);
+        resolve(sheet);
+      })();
+    });
+  } catch {
+    // eslint-disable-next-line no-console
+    console.warn(`Could not load ${path}`);
+  }
+  return cache[path];
+};
