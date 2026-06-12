bb8b601582d3063f2bae884af0fa8caa7b51956f
kozmaadrian <kozmaadrian@gmail.com>
2026-06-12 15:12:27 +0200

    feat(nx2-form): add sl-button field with S2 variants

---
+++ nx2/blocks/form/fields/button.js
@@ -0,0 +1,35 @@
+import { LitElement, html } from 'da-lit';
+import { loadStyle } from '../../../utils/utils.js';
+
+const style = await loadStyle(import.meta.url);
+
+class SlButton extends LitElement {
+  static properties = {
+    disabled: { type: Boolean, reflect: true },
+    // Spectrum variant: 'accent' (default) or 'secondary'.
+    variant: { reflect: true },
+  };
+
+  constructor() {
+    super();
+    this.variant = 'accent';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.shadowRoot.adoptedStyleSheets = [style];
+  }
+
+  render() {
+    // Disabled <button> swallows the click; callers bind @click on the host.
+    return html`
+      <button class="sl-button" type="button" ?disabled=${this.disabled}>
+        <slot></slot>
+      </button>
+    `;
+  }
+}
+
+if (!customElements.get('sl-button')) customElements.define('sl-button', SlButton);
+
+export default SlButton;
