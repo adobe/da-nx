51913743b8b867f4dcc1899864b298f3f3b59603
svinod <svinod@adobe.com>
2026-06-29 11:28:22 +0200

    fix: extend breadcrumb

--- nx2/blocks/shared/breadcrumb/breadcrumb.js
+++ nx2/blocks/shared/breadcrumb/breadcrumb.js
@@ -8,11 +8,19 @@ export default class NxBreadcrumb extends LitElement {
   static properties = {
     pathSegments: { type: Array, attribute: false },
     baseUrl: { type: String, attribute: false },
+    _hasSuffix: { state: true },
   };
 
   connectedCallback() {
     super.connectedCallback();
     this.shadowRoot.adoptedStyleSheets = [style];
+  }
+
+  firstUpdated() {
+    const slot = this.shadowRoot.querySelector('slot[name="suffix"]');
+    slot.addEventListener('slotchange', () => {
+      this._hasSuffix = slot.assignedElements().length > 0;
+    });
   }
 
   render() {
@@ -24,11 +32,15 @@ export default class NxBreadcrumb extends LitElement {
         <ol>
           ${crumbs.map((c, i) => html`
             <li class="crumb">
-                  ${i === crumbs.length - 1
-        ? html`<span class="current" aria-current="page">${c.label}</span>`
-        : html`<a href="${c.href}">${c.label}</a>`}
+              ${i === crumbs.length - 1
+                ? html`<span class="current" aria-current="page">${c.label}</span>
+                       <slot name="leaf"></slot>`
+                : html`<a href="${c.href}">${c.label}</a>`}
             </li>
           `)}
+          <li class="crumb crumb-suffix ${this._hasSuffix ? '' : 'hide'}">
+            <slot name="suffix"></slot>
+          </li>
         </ol>
       </nav>
     `;
