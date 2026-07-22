96298433be206be85a940f4fc40afd384e1ed883
Ramachandra Avuthu <ravuthu@adobe.com>
2026-07-22 10:49:31 -0400

    revert IIFE on loadStyle

--- nx2/scripts/nx.js
+++ nx2/scripts/nx.js
@@ -335,28 +335,27 @@ export async function loadArea({ area } = { area: document }) {
   }
 }
 
-export const loadStyle = (() => {
-  const cache = {};
+const cache = {};
 
-  return (supplied) => {
-    // Convenience replacement for WCs
-    const path = supplied.replace('.js', '.css');
+// eslint-disable-next-line import/prefer-default-export
+export const loadStyle = (supplied) => {
+  // Convenience replacement for WCs
+  const path = supplied.replace('.js', '.css');
 
-    try {
-      cache[path] ??= new Promise((resolve) => {
-        (async () => {
-          const resp = await fetch(path);
-          const text = await resp.text();
-          const sheet = new CSSStyleSheet({ baseURL: path });
-          sheet.path = path;
-          sheet.replaceSync(text);
-          resolve(sheet);
-        })();
-      });
-    } catch {
-      // eslint-disable-next-line no-console
-      console.warn(`Could not load ${path}`);
-    }
-    return cache[path];
-  };
-})();
+  try {
+    cache[path] ??= new Promise((resolve) => {
+      (async () => {
+        const resp = await fetch(path);
+        const text = await resp.text();
+        const sheet = new CSSStyleSheet({ baseURL: path });
+        sheet.path = path;
+        sheet.replaceSync(text);
+        resolve(sheet);
+      })();
+    });
+  } catch {
+    // eslint-disable-next-line no-console
+    console.warn(`Could not load ${path}`);
+  }
+  return cache[path];
+};
