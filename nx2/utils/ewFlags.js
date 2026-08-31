a626058cddb592c7693ef66541cdff2bc44db3f9
Sharanya Vinod <sharanyavinod90@gmail.com>
2026-08-31 09:14:25 +0200

    feat: override manifest (#697)
    * feat: override manifest
    
    * fix: update config + query
    
    * chore: code cleanup

--- nx2/utils/ewFlags.js
+++ nx2/utils/ewFlags.js
@@ -162,3 +162,9 @@ export async function isCoworkerEnabled({ org, site }) {
   const flags = await getEWFlags({ org, site });
   return flags['ew.coworker'] === 'true';
 }
+
+// See docs/chat-ao-component.md#manifest-override.
+export async function getManifestId({ org, site }) {
+  const flags = await getEWFlags({ org, site });
+  return flags['ew.coworkerManifest'] || null;
+}
